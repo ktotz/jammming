fdd2d6055037a956a0a034cdac38e32ae71c0ab6
Khamil <ktotharam@gmail.com>
2024-09-12 17:12:21 -0400

    added components and background/icon changes to site

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from  'react';
+import SearchBar from '../SearchBar/SearchBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        {<SearchBar />}
+        <div className='App-playlist'>
+
+
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
